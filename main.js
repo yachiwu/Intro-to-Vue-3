364031676645f0534518e54f7a1a86bac46e72e0
吳 蕥綺 <n0975116268@gmail.com>
2024-02-06 14:43:09 +0800

    L3 practice v-bind

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image : './assets/images/socks_green.jpg',
+            url : 'https://www.vuemastery.com/',
+            isActive : false
+        }
+    },
+    methods: {
+        handleClick() {
+            this.isActive = !this.isActive;
+        }
+    }
+})
