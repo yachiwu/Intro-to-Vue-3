05fdd5de9b1deda849135b92988b2e35760be0a5
吳 蕥綺 <n0975116268@gmail.com>
2024-02-06 15:33:16 +0800

    L4 practice v-if

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inventory : 8,
+            onSale: true
+        }
+    }
+})
